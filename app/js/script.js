2775d439b1fcc1454bd4b2e8fc9ea28ea0df3889
Arthur Correia <arthurcorria0109@gmail.com>
2023-02-27 15:24:18 -0300

    finalizando

--- app/js/script.js
+++ app/js/script.js
@@ -1,36 +1,53 @@
-function clearDisplayer(displayer) { displayer.innerHTML = ``; };
+function backspace(display) { return display.slice(0, display.length - 1); };
 
-function solve(calculation) { return eval(calculation) };
+function clearDisplayer(display) { display.innerHTML = ``; };
+
+function solve(calculation) { return eval(calculation); };
 
 let calculation = '';
+let displayingResult = false;
 function main(event) {
     let element = event.target.id;
-    let displayer = window.document.getElementById('displayer');
+    let display = window.document.getElementById('displayer');
 
     switch (element) {
         case 'equal':
             calculation = solve(calculation);
-            displayer.innerHTML = solve(calculation);
+            display.innerHTML = solve(calculation);
+            displayingResult = true;
             break;
-        case 'ac': 
-            displayer.innerHTML = ``;
+        
+        case 'ac':
+            display.innerHTML = ``;
             calculation = '';
             break;
-        case 'x': 
-            console.log('delete');
+        
+        case 'backspace': 
+            calculation = backspace(display.innerHTML);
+            display.innerHTML = backspace(display.innerHTML);
             break;
+        
         case '/': 
         case '*':
         case '+':
         case '-':
-            clearDisplayer(displayer);
-            calculation += element;
+            if (!displayingResult) {
+                clearDisplayer(display);
+                calculation += element;
+            }
             break;
+            
         case 'table':
             break;
+        
         default:
+            if (displayingResult) {
+                display.innerHTML = '';
+                calculation = '';
+            }
             calculation += element;
-            displayer.innerHTML += `${element}`;
+            display.innerHTML += `${element}`;
+            displayingResult = false;
             break;
     }
-}
+};
